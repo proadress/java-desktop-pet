bb1675e75e26e4baef2d84ae7d31a0153307502d
Yupoer <qzahamada@gmail.com>
2024-05-23 03:43:32 +0800

    delete javafx

--- deskpet_dash/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.deskpet_dash {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.deskpet_dash to javafx.fxml;
-    exports org.example.deskpet_dash;
-}
